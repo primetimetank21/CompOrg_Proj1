651cc38b1abe1bec1edc5ca1186022092cb91673
Earl Tankard, Jr <theflashtank21@gmail.com>
2019-10-20 02:52:30 -0400

    MAJOR rework... back to using string input && using char as a 'buffer'

--- project1.s
+++ project1.s
@@ -6,25 +6,25 @@
 #  M = 21
 
 .data
-	base_N:  	.word 31	# base-N number from calculation
-	Mth_num: 	.word 21	# Mth number from calculation
-	#str:	 	.space 6	# 5 bytes for base-N chars, 1 byte for newline and NULL chars
-	_word:	 	.space 5	# 4 bytes for each int, 1 byte for newline and NULL chars
-	array:		.space 6	# 5 bytes for char values, 1 byte for NULL char (only need 4 bits) 
-	#inputCount: 	.word 0
+	array:		.space 11	#10 element array (1 NULL space)
+	base_N:  	.word 31	#base-N number from calculation
+	char: 		.space 2	#1 byte for char, 1 byte for NULL
+	Mth_num: 	.word 21	#Mth number from calculation
+	#_word:	 	.space 5	#4 bytes for each int, 1 byte for newline and NULL chars
 
 .text
-	addi $s0, $zero, 0	#register for tracking input loop value
-
 	main:
-		li $v0, 12		# read_char command
-		syscall
+		la $s1, array       	#set base address of array to s1
 
-		addi $s0, $s0, 1	#increments counter	
+		li $v0, 8       	#call code for read string
+		la $a0, char    	#load address of char for read
+		li $a1, 2      		#length of string is 1byte char and 1byte for null
+		syscall         	#store the char byte from input buffer into char
 
-		bne $s0, 10, main	#exit condition
-		j exit
-
+		lb $t0, char        	#load the char from char buffer into t0, stripping null
+		sb $t0, 0($s1)      	#store the char into the nth elem of array
+		addi $s1, $s1, 1    	#increments base address of array
+		
 	exit:
-		li $v0, 10		# exit_program
+		li $v0, 10		#exit_program
 		syscall
