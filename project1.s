3c314c3a68c8aabc063a39d2ddfa6f76636cc8a9
Earl Tankard, Jr <theflashtank21@gmail.com>
2019-10-23 04:37:13 -0400

    Removed unnecessary .data && checked some test cases. One of the final commits (I think)

--- project1.s
+++ project1.s
@@ -8,15 +8,11 @@
 .data
 	array:		.space 40	#10 element array (1 NULL space)
 	array_size:	.word 10	#10 elements in array
-	base_N:  	.word 31	#base-N number from calculation
 	char: 		.space 2	#1 byte for char, 1 byte for NULL
-	Mth_num: 	.word 21	#Mth number from calculation
 	newline:	.asciiz "\n"	#newline character
-	ten:		.word 10	#value of 10 (decimal)
-	#_word:	 	.space 5	#4 bytes for each int, 1 byte for newline and NULL chars
 
 .text
-	main:
+	main:				#main label
 		addi $s0, $zero, 0	#register for tracking input loop value
 		la $s1, array       	#set base address of array to s1
 
@@ -36,10 +32,10 @@
 			addi $s0, $s0, 1	#increment counter
 			jr $ra          	#jump back (into loop label)
 
-	convert:
-		li $v0, 4
-		la $a0, newline
-		syscall
+	convert:			#converts each char from ascii => decimal (based on my base-N number)
+		li $v0, 4		#print_string command
+		la $a0, newline		#loads address of newline
+		syscall			#prints '\n'
 		
 		move $s0, $zero		#reset counter to 0
 		lw $t4, array_size	#$t4 = size
@@ -47,9 +43,8 @@
 		sll $t1, $t4, 2		#$t1 = size * 4
 		add $t2, $t0, $t1	#$t2 = address of array[size] (aka &array[size])
 		move $s1, $zero		#$s1 = 0 (and will serve as a total sum)
-		lw $s2, ten		#$s2 = 10
 
-		add_char:
+		add_char:			#adds each char to the total sum (after converting the char)
 			beq $s0, 10, exit	#if $s0 == 10, jump to exit
 			lw $t3, 0($t0)		#$t3 = array[i]
 			blt $t3, 48, _zero	#if array[i] < 48, jump to _zero
@@ -58,36 +53,33 @@
 			add $s1, $s1, $t3	#adds $t3 to total sum
 
 			
-			_return:
+			_return:			#increments counter && address
 			addi $t0, $t0, 4		#$t0 = $t0 + 4
 			addi $s0, $s0, 1		#increment counter
 			j add_char			#jump back to add_char		
 		
 
-				_zero:
+				_zero:				#sets values to zero
 					move $t3, $zero  	#array[i] = 0
 					j _return	  	#jumps to _return
 
-				check_upper:
+				check_upper:			#checks if char's ascii value is within the uppercase alphabet range
 					blt $t3, 65, _zero	#if array[i] < 65, jump to _zero 
 					bgt $t3, 85, check_lower#if array[i] > 85, jump to check_lower
 					addi $t3, $t3, -64	#else, $t3 = array[i] - 64
-					add $t3, $t3, $s2	#$t3 = $t3 + 10
+					addi $t3, $t3, 9	#$t3 = $t3 + 9
 					add $s1, $s1, $t3	#adds $t3 to total sum
-
 					j _return	  	#jumps to _return
 
-				check_lower:
+				check_lower:			#checks if char's ascii value is within the lowercase alphabet range
 					blt $t3, 97,  _zero	#if array[i] < 97, jump to _zero 
 					bgt $t3, 117, _zero	#if array[i] > 117, jump to _zero 
 					addi $t3, $t3, -96	#else, $t3 = array[i] - 96
-					add $t3, $t3, $s2	#$t3 = $t3 + 10					
+					addi $t3, $t3, 9	#$t3 = $t3 + 9				
 					add $s1, $s1, $t3	#adds $t3 to total sum
 					j _return		#jumps to _return
 	
-	
-	
-	exit:
+	exit:				#finishes the program
 		li $v0, 1		#print_int command
 		move $a0, $s1		#$a0 = total sum
 		syscall			#prints total sum
