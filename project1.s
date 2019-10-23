4d1eae7393a0468aaf9c56eacbc25665dfcdee66
Earl Tankard, Jr <theflashtank21@gmail.com>
2019-10-23 03:53:13 -0400

    Finished conditionals && adding total sum

--- project1.s
+++ project1.s
@@ -12,6 +12,7 @@
 	char: 		.space 2	#1 byte for char, 1 byte for NULL
 	Mth_num: 	.word 21	#Mth number from calculation
 	newline:	.asciiz "\n"	#newline character
+	ten:		.word 10	#value of 10 (decimal)
 	#_word:	 	.space 5	#4 bytes for each int, 1 byte for newline and NULL chars
 
 .text
@@ -46,6 +47,7 @@
 		sll $t1, $t4, 2		#$t1 = size * 4
 		add $t2, $t0, $t1	#$t2 = address of array[size] (aka &array[size])
 		move $s1, $zero		#$s1 = 0 (and will serve as a total sum)
+		lw $s2, ten		#$s2 = 10
 
 		add_char:
 			beq $s0, 10, exit	#if $s0 == 10, jump to exit
@@ -53,14 +55,14 @@
 			blt $t3, 48, _zero	#if array[i] < 48, jump to _zero
 			bgt $t3, 57, check_upper#if array[i] > 57, jump to check_upper
 			addi $t3, $t3, -48	#else, $t3 = array[i] - 48
+			add $s1, $s1, $t3	#adds $t3 to total sum
+
 			
 			_return:
-			
-			add $s1, $s1, $t3		#adds $t3 to total sum
 			addi $t0, $t0, 4		#$t0 = $t0 + 4
 			addi $s0, $s0, 1		#increment counter
 			j add_char			#jump back to add_char		
-			
+		
 
 				_zero:
 					move $t3, $zero  	#array[i] = 0
@@ -70,11 +72,18 @@
 					blt $t3, 65, _zero	#if array[i] < 65, jump to _zero 
 					bgt $t3, 85, check_lower#if array[i] > 85, jump to check_lower
 					addi $t3, $t3, -64	#else, $t3 = array[i] - 64
+					add $t3, $t3, $s2	#$t3 = $t3 + 10
+					add $s1, $s1, $t3	#adds $t3 to total sum
+
 					j _return	  	#jumps to _return
 
 				check_lower:
-					
-					j _return
+					blt $t3, 97,  _zero	#if array[i] < 97, jump to _zero 
+					bgt $t3, 117, _zero	#if array[i] > 117, jump to _zero 
+					addi $t3, $t3, -96	#else, $t3 = array[i] - 96
+					add $t3, $t3, $s2	#$t3 = $t3 + 10					
+					add $s1, $s1, $t3	#adds $t3 to total sum
+					j _return		#jumps to _return
 	
 	
 	
