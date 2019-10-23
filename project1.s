2ec19ec96df452146d0dbd31ea99cd30a0e00781
Earl Tankard, Jr <theflashtank21@gmail.com>
2019-10-23 00:00:14 -0400

    Added array_size && initialize/increment the address pointed to

--- project1.s
+++ project1.s
@@ -7,6 +7,7 @@
 
 .data
 	array:		.space 11	#10 element array (1 NULL space)
+	array_size:	.word 10	#10 elements in array
 	base_N:  	.word 31	#base-N number from calculation
 	char: 		.space 2	#1 byte for char, 1 byte for NULL
 	Mth_num: 	.word 21	#Mth number from calculation
@@ -25,20 +26,27 @@
 		addi $s1, $s1, 1    	#increments base address of array (get to next element location)
 		j loop              	#jump to start loop
 
-	getChar:           		#read char from keyboard buffer and return ascii value
-		li $v0, 8       	#read_string command
-		la $a0, char    	#load address of char for read
-		li $a1, 2      		#length of string is 1byte char and 1byte for null
-		syscall         	#store the char byte from input buffer into char
-		addi $s0, $s0, 1	#increment counter
-		jr $ra          	#jump back (into loop label)
+		getChar:           		#read char from keyboard buffer and return ascii value
+			li $v0, 8       	#read_string command
+			la $a0, char    	#load address of char for read
+			li $a1, 2      		#length of string is 1byte char and 1byte for null
+			syscall         	#store the char byte from input buffer into char
+			addi $s0, $s0, 1	#increment counter
+			jr $ra          	#jump back (into loop label)
 
 	convert:
-		addi $s0, $zero, 0	#reset counter to 0
-	loop2:
-		beq $s0, 10, exit	#if $s0 == 10, jump to exit
-		addi $s0, $s0, 1	#increment counter
-		j loop2			#jump back to loop2		
+		move $s0, $zero		#reset counter to 0
+		lw $t4, array_size	#$t4 = size
+		la $t0, array		#$t0 = address of array[0] (aka &array[0])
+		sll $t1, $t4, 2		#$t1 = size * 4
+		add $t2, $t0, $t1	#$t2 = address of array[size] (aka &array[size])
+
+		loop2:
+			lw $t3, 0($t0)		#$t3 = array[i]
+			addi $t0, $t0, 4	#$t0 = $t0 + 4
+			beq $s0, 10, exit	#if $s0 == 10, jump to exit
+			addi $s0, $s0, 1	#increment counter
+			j loop2			#jump back to loop2		
 	
 	
 	
